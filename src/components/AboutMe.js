40c4d37adf8d35bd4c507de66a6c583a1a83d9da
isabelochoaco <79860946+isabelochoaco@users.noreply.github.com>
2021-08-30 18:18:31 +0200

    About Me Completed

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,10 +6,12 @@ const AboutMe = () => {
         <div className="container py-5">
             <div className="row">
                 <div className="col-lg-6 col-xm-12">
-                    <img src={author} alt="author.." />
+                    <div className="photo-wrap mb-5">
+                    <img className="profile-img" src={author} alt="author.." />                        
+                    </div>
                 </div>
                 <div className="col-lg-6 col-xm-12">
-                    <h1>About Me</h1>
+                    <h1 className="about-heading">About Me</h1>
                     <p>
                         B. degree in Hotel Management and Tourism, with over ten years' experience in different fields and countries where I've been developing my professional skills. I am continuously seeking to improve my knowledge, where allows me to increase my expertise and abilities while working towards achieving my goals. 
                     </p>
