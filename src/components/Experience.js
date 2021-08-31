a81a8e92fc0dd32bdb139141072e87c1bcfb25f2
isabelochoaco <79860946+isabelochoaco@users.noreply.github.com>
2021-08-31 19:00:21 +0200

    Experience component completed

---
+++ src/components/Experience.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+const Experience = () => {
+    return (
+        <div className="experience">
+            <div className="d-flex justify-content-center my-5">
+                <h1>experience</h1>
+            </div>
+            <div className="container experience-wrapper">
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2014-2015</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus auctor sem nisi, nec accumsan libero sodales ut. Mauris a nisl cursus, bibendum est quis, semper dolor. Integer tincidunt ac metus elementum facilisis.</p>
+                    </div>
+                </div>
+                {/* - */}
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2015-2016</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus auctor sem nisi, nec accumsan libero sodales ut. Mauris a nisl cursus, bibendum est quis, semper dolor. Integer tincidunt ac metus elementum facilisis.</p>
+                    </div>
+                </div>
+                {/* - */}
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2017-2018</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus auctor sem nisi, nec accumsan libero sodales ut. Mauris a nisl cursus, bibendum est quis, semper dolor. Integer tincidunt ac metus elementum facilisis.</p>
+                    </div>
+                </div>
+                {/* - */}
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2018-2019</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus auctor sem nisi, nec accumsan libero sodales ut. Mauris a nisl cursus, bibendum est quis, semper dolor. Integer tincidunt ac metus elementum facilisis.</p>
+                    </div>
+                </div>
+                {/* - */}
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2019-2020</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus auctor sem nisi, nec accumsan libero sodales ut. Mauris a nisl cursus, bibendum est quis, semper dolor. Integer tincidunt ac metus elementum facilisis.</p>
+                    </div>
+                </div>
+                {/* - */}
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2020-2021</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus auctor sem nisi, nec accumsan libero sodales ut. Mauris a nisl cursus, bibendum est quis, semper dolor. Integer tincidunt ac metus elementum facilisis.</p>
+                    </div>
+                </div>
+            </div>                          
+        </div>
+    )
+}
+
+export default Experience
